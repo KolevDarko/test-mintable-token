6e6b411dccba5f4c4ff80031ecf3b962ccb580ad
darko <kolevdarko@gmail.com>
2021-05-20 16:25:33 +0200

    Updates

---
+++ precache-manifest.b2001bc2a2e8bff1d3259f0070bcd03c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372e136173fbce5da70855268a5772d4",
+    "url": "/test-mintable-token/index.html"
+  },
+  {
+    "revision": "62c722caeb8639f56f2f",
+    "url": "/test-mintable-token/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e53040e60329839346be",
+    "url": "/test-mintable-token/static/js/2.7b0ff469.chunk.js"
+  },
+  {
+    "revision": "62c722caeb8639f56f2f",
+    "url": "/test-mintable-token/static/js/main.3d3382a5.chunk.js"
+  },
+  {
+    "revision": "2a4cb6e16ae01a320afd",
+    "url": "/test-mintable-token/static/js/runtime-main.981218d4.js"
+  }
+]);
